a0df89dd4da643b4eb02c4b6c74d392fa536fe3c
Amarumarcus <144006456+Amarumarcus@users.noreply.github.com>
2024-02-13 13:13:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,96 +1,93 @@
-//javascript
 
-let rangeValue = document.getElementById("rangeValue")
-let customRange1 = document.getElementById("customRange1")
-let btn1 = document.getElementById("btn1")
-let btn2 = document.getElementById("btn2")
-let btn3 = document.getElementById("btn3")
-let btn4 = document.getElementById("btn4")
-let chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "@", "#", "%", "^", "&", "*", "(", ")", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-let newRangeValue = [8]
+const charSmall = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let characters = charSmall
+const charBig = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const charNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const charSpec = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+const pass1 = document.getElementById("pass1")
+const pass2 = document.getElementById("pass2")
+let big = document.getElementById("big")
+let num = document.getElementById("num")
+let spec = document.getElementById("spec")
+const genBtn = document.getElementById("gen_btn")
+let passwordLength = [8]
 
-// Values from range
 function getRangeValue() {
-    newRangeValue = ""
+    passwordLength = ""
     let sliderValue = document.getElementById("customRange1").value;
     document.getElementById("rangeValue").innerHTML = sliderValue;
-    newRangeValue = sliderValue
+    passwordLength = sliderValue
 }
 
-// Password generator
- function genPass() {
-        password = ""
-        for (let i = 0; i < newRangeValue; i++) {
-        let randomIndex = Math.floor(Math.random() * chars.length)
-        password += chars[randomIndex]
+// function addChar() {
+//     if (big.checked === true && num.checked === true) {
+//         characters = charSmall.concat(charBig)
+//         console.log(characters)
+//     } else {
+//         characters = charSmall
+//         console.log(characters)
+//     }
+// }
+
+function addChar() {
+    if (big.checked === true && num.checked === true && spec.checked === true) {
+        characters = charSmall.concat(charBig, charNum, charSpec)
+    } else if (big.checked === true && num.checked === true && spec.checked === false) {
+        characters = charSmall.concat(charBig, charNum)
+    } else if (big.checked === true && num.checked === false && spec.checked === true) {
+        characters = charSmall.concat(charBig, charSpec)
+    } else if (big.checked === false && num.checked === true && spec.checked === true) {
+        characters = charSmall.concat(charNum, charSpec)
+    } else if (big.checked === true && num.checked === false && spec.checked === false) {
+        characters = charSmall.concat(charBig)
+    } else if (big.checked === false && num.checked === true && spec.checked === false) {
+        characters = charSmall.concat(charNum)
+    } else if (big.checked === false && num.checked === false && spec.checked === true) {
+        characters = charSmall.concat(charSpec)
+    } else {
+        characters = charSmall
     }
-   return password
 }
 
-// Password to buttons
-gen.addEventListener("click", function getPasses() {
-    btn1.textContent = genPass()
-    btn2.textContent = genPass()
-    btn3.textContent = genPass()
-    btn4.textContent = genPass()
+function genPass() {
+    pass = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomIndex = Math.floor ( Math.random() * characters.length)
+        pass += characters[randomIndex]
+    }
+    return pass
+}
+
+genBtn.addEventListener("click", function() {
+    pass1.textContent = genPass()
+    pass2.textContent = genPass()
 })
 
-// Copy to clipbord buttons
-
-function copy1() {
-  const element1 = document.querySelector('#btn1');
-  const storage1 = document.createElement('textarea');
-  storage1.value = element1.innerHTML;
-  element1.appendChild(storage1);
-  storage1.select();
-  storage1.setSelectionRange(0, 99999);
-  document.execCommand('copy');
-  element1.removeChild(storage1);
+pass1.addEventListener("click", function() {
+  const pass1 = document.getElementById("pass1").innerText
+  const el = document.createElement("textarea")
+  el.value = pass1
+  el.setAttribute('readonly', '')
+  el.style.position = 'absolute'
+  el.style.left = '-9999px'
+  document.body.appendChild(el)
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
   alert(`Password copied to clipboard!`);
-}
+})
 
-function copy2() {
-  const element1 = document.querySelector('#btn2');
-  const storage1 = document.createElement('textarea');
-  storage1.value = element1.innerHTML;
-  element1.appendChild(storage1);
-  storage1.select();
-  storage1.setSelectionRange(0, 99999);
-  document.execCommand('copy');
-  element1.removeChild(storage1);
+pass2.addEventListener("click", function() {
+  const pass2 = document.getElementById("pass2").innerText
+  const el = document.createElement("textarea")
+  el.value = pass2
+  el.setAttribute('readonly', '')
+  el.style.position = 'absolute'
+  el.style.left = '-9999px'
+  document.body.appendChild(el)
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
   alert(`Password copied to clipboard!`);
-}
-
-function copy3() {
-  const element1 = document.querySelector('#btn3');
-  const storage1 = document.createElement('textarea');
-  storage1.value = element1.innerHTML;
-  element1.appendChild(storage1);
-  storage1.select();
-  storage1.setSelectionRange(0, 99999);
-  document.execCommand('copy');
-  element1.removeChild(storage1);
-  alert(`Password copied to clipboard!`);
-}
-
-function copy4() {
-  const element1 = document.querySelector('#btn4');
-  const storage1 = document.createElement('textarea');
-  storage1.value = element1.innerHTML;
-  element1.appendChild(storage1);
-  storage1.select();
-  storage1.setSelectionRange(0, 99999);
-  document.execCommand('copy');
-  element1.removeChild(storage1);
-  alert(`Password copied to clipboard!`);
-}
-
-// Enable buttons for copy
-
-gen.addEventListener("click", function enableBnt() {
-  btn1.removeAttribute('disabled');
-  btn2.removeAttribute('disabled');
-  btn3.removeAttribute('disabled');
-  btn4.removeAttribute('disabled');
 })
